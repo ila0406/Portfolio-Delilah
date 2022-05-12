3b12b47ac4188b0c79980e8d4f877a2264357b80
Delilah <ila0406@gmail.com>
2022-05-11 19:38:22 -0600

    imported images to component files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import MyImages from "./images/ME.jpg"
 import AboutMe from "./components/AboutMe/AboutMe"
 import Work from "./components/Work/Work"
 import Contact from "./components/Contact/Contact"
@@ -14,7 +15,7 @@ function App() {
         <header>
         <h1>Delilah Haas</h1>
         <Navbar />
-        <img class="me-image" src="/images/ME.jpg" alt="Delilah Haas"/>    
+        <img class="me-image" src={MyImages} alt="Delilah Haas"/>    
         </header>
         <div id="hero-image" title="Night sky at dusk - banner image"></div>    
       
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,3 +1,8 @@
+import ImgProject1 from "./travel-trustees.png"
+import ImgProject2 from "./tttp2.png"
+import ImgWeather from "./Weather-Dashboard.png"
+import ImgScheduler from "./Workday-Scheduler.png"
+
 function Work() {
     return  (
         <main class="container" id="work" >
@@ -5,29 +10,29 @@ function Work() {
             <section class="wrapper">
                 <div class="box">
                     <div class="projects">
-                        <img class="project-img" src="Weather-Dashboard.png" alt="Mockup for Weather dashboard"/>
+                        <img class="project-img" src={ImgWeather} alt="Mockup for Weather dashboard"/>
                         <a href="https://ila0406.github.io/Weather-Dashboard/" title="Homework 6 - Weather Dashboard"><h3>Weather Dashboard</h3></a>
                         <p>Find out today's weather and the forecast for the next five days</p>
                         <a href="https://github.com/ila0406/Weather-Dashboard" title="Code Repo"><h4>Code Repo</h4></a>
                     </div>
                     <div class="projects">
-                        <img class="project-img" src="Workday-Scheduler.png" alt="Mockup for Workday Scheduler"/>
+                        <img class="project-img" src={ImgScheduler} alt="Mockup for Workday Scheduler"/>
                         <a href="https://ila0406.github.io/Work-Day-Scheduler/" title="Homework 5 - Work Day Scheduler"><h3>Work Day Scheduler</h3></a>
                         <p>Keep track of your important events at work</p>
                         <a href="https://github.com/ila0406/Work-Day-Scheduler" title="Code Repo"><h4>Code Repo</h4></a>
                     </div>
-                    {/* <div class="projects">
-                        <img class="project-img" src="tttp2.png"  alt="Mockup for Travel Trustees2" title="Project 1 - Travel Trustees">
+                    <div class="projects">
+                        <img class="project-img" src={ImgProject2}  alt="Mockup for Travel Trustees2"/>
                         <a href="https://ila0406.github.io/Travel-Trustees/" title="Project 1 - Travel Trustees"><h3>Travel Trustees</h3></a>
                         <p>With safety information and travel information all in one place, you can plan trips quickly and safely.</p>
                         <a href="https://github.com/ila0406/Travel-Trustees" title="Code Repo"><h4>Code Repo</h4></a>
                     </div>
                     <div class="projects">
-                        <img class="project-img" src="travel-trustees.png"  alt="Mockup for Travel Trustees" title="Project 1 - Travel Trustees v2">
+                        <img class="project-img" src={ImgProject1}  alt="Mockup for Travel Trustees"/>
                         <a href="https://immense-ravine-97579.herokuapp.com/" title="Project 2 - Travel Trustees v2"><h3>Travel Trustees</h3></a>
                         <p>With safety information and travel information all in one place, you can plan trips quickly and safely.</p>
                         <a href="https://github.com/ila0406/Travel-Trustees-2" title="Code Repo"><h4>Code Repo</h4></a>
-                    </div> */}
+                    </div>
                 </div>
             </section>
         </main>
